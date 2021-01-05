6b0e29e8c1f34786abaa7d896d55c5eb8d386410
Amir Fefer <amirfefer@gmail.com>
2021-01-05 13:39:14 +0200

    Add React

--- app/controllers/foreman_plugin_template/hosts_controller.rb
+++ app/controllers/foreman_plugin_template/hosts_controller.rb
@@ -2,10 +2,15 @@ module ForemanPluginTemplate
   # Example: Plugin's HostsController inherits from Foreman's HostsController
   class HostsController < ::HostsController
     # change layout if needed
-    # layout 'foreman_plugin_template/layouts/new_layout'
+    layout 'foreman_plugin_template/layouts/new_layout'
 
     def new_action
       # automatically renders view/foreman_plugin_template/hosts/new_action
+    end
+
+    def react_template_page_description
+      render json: { description: "This is an example for a full react page in a foreman's plugin.
+         For further documentation please visit foreman's Storybook page or run it locally by running 'npm run stories'" }
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
-Rails.application.routes.draw do
-  get 'new_action', to: 'foreman_plugin_template/hosts#new_action'
+ForemanPluginTemplate::Engine.routes.draw do
+  get 'new_action', to: 'hosts#new_action'
+  get 'plugin_template_description', to: 'hosts#react_template_page_description'
+  get 'welcome', to: '/react#index'
+end
+
+Foreman::Application.routes.draw do
+  mount ForemanPluginTemplate::Engine, at: '/foreman_plugin_template'
 end
